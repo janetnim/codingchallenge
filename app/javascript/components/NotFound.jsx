435525a599300abfd7aa0c318d0288d6ce33d9e3
Janet Wairimu <janetnim401@gmail.com>
2021-02-05 17:07:34 +0300

    export default not found component, center image

--- app/javascript/components/NotFound.jsx
+++ app/javascript/components/NotFound.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import NotFoundImage from '../assets/images/404.jpeg';
 
-export const NotFound = () => {
+const NotFound = () => {
   return (
     <div className="not-found">
       <img src={NotFoundImage} alt="Not Found" />
     </div>
   );
 }
+
+export default NotFound;
